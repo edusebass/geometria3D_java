ffd19fdb1582bbc9e8d50cc66eb198e42af4f46e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-06 10:11:42 -0500

    volumen piramide

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        figuras3D cubo = new figuras3D();
-        figuras3D piramdes = new figuras3D();
-        figuras3D esferas = new figuras3D();
-        figuras3D cilindro = new figuras3D();
+        figuras3D piramides = new figuras3D(0, 0, 0);
+        piramides.setAreaBase(4.0);
+        piramides.setLadoBase(5.24);
+        piramides.setAltura(4.21);
+
+        System.out.println("Volumen piramide: " + piramides.getVolumenPiramide());
+
+
+
     }
 }
--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,10 +1,47 @@
 public class figuras3D {
-    float lado; //cubo
-    float areaBase, altura; //piramide
+    private double areaBase, ladoBase, altura;
+    private double volumenPiramide;
 
-    float radioesfera; //esfera
+    //volumen piramide
+    public figuras3D(double areaBase, double ladoBase, double altura) {
+        this.areaBase = areaBase;
+        this.ladoBase = ladoBase;
+        this.altura = altura;
+    }
 
-    float radioCili, alturaCili; //cilindro
-    double PI = 3.14;
+    public double getAreaBase() {
+        return areaBase;
+    }
+    public void setAreaBase(double areaBase) {
+        this.areaBase = areaBase;
+    }
+
+    public double getLadoBase() {
+        return ladoBase;
+    }
+    public void setLadoBase(double ladoBase) {
+        this.ladoBase = ladoBase;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getVolumenPiramide() {
+        return volumenPiramide;
+    }
+
+    private void setVolumenPiramide(double volumenPiramide) {
+        this.volumenPiramide = volumenPiramide(getAreaBase(), getLadoBase(), getAltura());
+    }
+
+    private double volumenPiramide(double areaBase, double ladoBase, double altura) {
+        volumenPiramide = (1/3 * areaBase * ladoBase * altura);
+        return volumenPiramide;
+    }
+
 
 }
