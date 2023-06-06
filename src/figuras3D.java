634ad11362dba7d0740e97da91ae08b494f84021
edusebass <eduardoalmachi123@hotmail.com>
2023-06-06 15:37:04 -0500

    volumen piramide

--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,10 +1,49 @@
 public class figuras3D {
-    float lado; //cubo
-    float areaBase, altura; //piramide
+    private double areaBase, ladoBase, altura;
+    private double volumenPiramide;
 
-    float radioesfera; //esfera
+    //volumen piramide
+    public figuras3D(double areaBase, double ladoBase, double altura) {
+        this.areaBase = areaBase;
+        this.ladoBase = ladoBase;
+        this.altura = altura;
+    }
 
-    float radioCili, alturaCili; //cilindro
-    double PI = 3.14;
+    public double getAreaBase() {
+        return areaBase;
+    }
+    public void setAreaBase(double areaBase) {
+        this.areaBase = areaBase;
+        setVolumenPiramide();
+    }
 
+    public double getLadoBase() {
+        return ladoBase;
+    }
+    public void setLadoBase(double ladoBase) {
+        this.ladoBase = ladoBase;
+        setVolumenPiramide();
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+        setVolumenPiramide();
+    }
+
+    public double getVolumenPiramide() {
+        return volumenPiramide;
+    }
+
+    private void setVolumenPiramide() {
+        this.volumenPiramide = volumenPiramide(getAreaBase(), getLadoBase(), getAltura());
+    }
+
+    private double volumenPiramide(double areaBase, double ladoBase, double altura) {
+        volumenPiramide = (1.0 / 3.0) * areaBase * ladoBase * altura;
+        return volumenPiramide;
+    }
 }
+
