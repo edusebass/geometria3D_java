8b44e13c77401d7da1e930e3b99f2a40292c85c7
Gilmar_Morales <gilmarmoralesriva2003@gmail.com>
2023-06-06 09:53:23 -0500

    Volumen cubo

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        figuras3D cubo = new figuras3D();
-        figuras3D piramdes = new figuras3D();
-        figuras3D esferas = new figuras3D();
-        figuras3D cilindro = new figuras3D();
+        figuras3D cubo = new figuras3D(3);
+        figuras3D piramdes = new figuras3D(0);
+        figuras3D esferas = new figuras3D(0);
+        figuras3D cilindro = new figuras3D(0);
+
+        System.out.println("VOLUMEN CUBO: "+ cubo.volCubo(0));
+
     }
 }
--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,10 +1,40 @@
 public class figuras3D {
-    float lado; //cubo
+    double lado; //cubo
+   private double volCubo;
     float areaBase, altura; //piramide
 
     float radioesfera; //esfera
 
     float radioCili, alturaCili; //cilindro
     double PI = 3.14;
+
+    //Constructor cubo
+    public figuras3D(float lado) {
+        this.lado = lado;
+    }
+
+    //SETTERS Y GETTERS CUBO
+
+    private double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getVolCubo() {
+        return volCubo;
+    }
+
+    private void setVolCubo(double volCubo) {
+        this.volCubo = volCubo;
+    }
+
+    //CALCULO VOLUMEN CUBO
+    public double volCubo(double lado){
+        volCubo=(getLado()*getLado()*getLado());
+        return this.volCubo;
+    };
 
 }
