d892b2d0fb452f2f6365ecec5245a3656e3977f0
Paul Hidalgo <paulesh5@gmail.com>
2023-06-06 09:55:59 -0500

    Volumen cilindro

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        figuras3D cubo = new figuras3D();
-        figuras3D piramdes = new figuras3D();
-        figuras3D esferas = new figuras3D();
-        figuras3D cilindro = new figuras3D();
+        figuras3D cubo = new figuras3D(0,0);
+        figuras3D piramdes = new figuras3D(0,0);
+        figuras3D esferas = new figuras3D(0,0);
+        figuras3D cilindro = new figuras3D(0,0);
+
+        cilindro.setAlturaCili(12);
+        cilindro.setRadioCili(5);
+
+        System.out.println("Volumen cilindro: " + cilindro.getVolumenCilindro());
+
     }
 }
--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,10 +1,45 @@
 public class figuras3D {
-    float lado; //cubo
-    float areaBase, altura; //piramide
+    private float lado; //cubo
+    private float areaBase, altura; //piramide
 
-    float radioesfera; //esfera
+    private float radioesfera; //esfera
 
-    float radioCili, alturaCili; //cilindro
-    double PI = 3.14;
+    private float radioCili, alturaCili; //cilindro
+    private double PI = 3.14;
+    private double volumenCilindro;
 
+    public figuras3D(float radioCili, float alturaCili) {
+        this.radioCili = radioCili;
+        this.alturaCili = alturaCili;
+    }
+
+    public float getRadioCili() {
+        return radioCili;
+    }
+
+    public void setRadioCili(float radioCili) {
+        this.radioCili = radioCili;
+    }
+
+    public float getAlturaCili() {
+        return alturaCili;
+    }
+
+    public void setAlturaCili(float alturaCili) {
+        this.alturaCili = alturaCili;
+    }
+
+    public double getVolumenCilindro() {
+        setVolumenCilindro();
+        return volumenCilindro;
+    }
+
+    private void setVolumenCilindro() {
+        this.volumenCilindro = volumenCilindro(getRadioCili(),getAlturaCili());
+    }
+
+    private double volumenCilindro(double radio, double altura1){
+        volumenCilindro = (PI * (radio*radio) * altura1);
+        return volumenCilindro;
+    }
 }
