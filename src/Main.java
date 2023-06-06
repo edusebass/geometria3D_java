a76b83f10fb029bb6cef3132c80c2b26aa45e554
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-06-06 09:18:02 -0500

    CREACION CLASE FIGURAS 3D

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        figuras3D cubo = new figuras3D();
+        figuras3D piramdes = new figuras3D();
+        figuras3D esferas = new figuras3D();
+        figuras3D cilindro = new figuras3D();
     }
 }
--- src/figuras3D.java
+++ src/figuras3D.java
@@ -1,2 +1,10 @@
 public class figuras3D {
+    float lado; //cubo
+    float areaBase, altura; //piramide
+
+    float radioesfera; //esfera
+
+    float radioCili, alturaCili; //cilindro
+    double PI = 3.14;
+
 }
